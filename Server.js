70bc909941b2700fcd638011f8072385c0bd977f
JadeDecker <jade.decker@googlemail.com>
2018-06-03 17:52:57 +0200

    Add files via upload

--- Server.js
+++ Server.js
@@ -3,28 +3,77 @@ const Http = require("http");
 const Url = require("url");
 var Server;
 (function (Server) {
+    let studiHomoAssoc = {};
     let port = process.env.PORT;
     if (port == undefined)
-        port = 8100;
-    let server = Http.createServer();
-    server.addListener("listening", handleListen);
+        port = 8200;
+    let server = Http.createServer((_request, _response) => {
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+    });
     server.addListener("request", handleRequest);
     server.listen(port);
-    function handleListen() {
-        console.log("Ich höre?");
-    }
     function handleRequest(_request, _response) {
         console.log("Ich höre Stimmen!");
         let query = Url.parse(_request.url, true).query;
-        let a = parseInt(query["a"]);
-        let b = parseInt(query["b"]);
-        for (let key in query)
-            console.log(query[key]);
-        _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write("Ich habe dich gehört<br/>");
-        _response.write("Das Ergebnis ist: " + (a + b));
+        console.log(query["command"]);
+        if (query["command"]) {
+            switch (query["command"]) {
+                case "insert":
+                    insert(query, _response);
+                    break;
+                case "refresh":
+                    refresh(_response);
+                    break;
+                case "search":
+                    search(query, _response);
+                    break;
+                default:
+                    error();
+            }
+        }
         _response.end();
+    }
+    function insert(query, _response) {
+        let obj = JSON.parse(query["data"]);
+        let _vorname = obj.vorname;
+        let _nachname = obj.nachname;
+        let _martrikelnummer = obj.martrikelnummer.toString();
+        let _alter = obj.alter;
+        let _geschlecht = obj.geschlecht;
+        let _studiengang = obj.studiengang;
+        let studi;
+        studi = {
+            vorname: _vorname,
+            nachname: _nachname,
+            martrikelnummer: parseInt(_martrikelnummer),
+            alter: _alter,
+            geschlecht: _geschlecht,
+            studiengang: _studiengang
+        };
+        studiHomoAssoc[_martrikelnummer] = studi;
+        _response.write("Daten empfangen");
+    }
+    function refresh(_response) {
+        console.log(studiHomoAssoc);
+        for (let matrikel in studiHomoAssoc) {
+            let studi = studiHomoAssoc[matrikel];
+            let line = '<tr><td class="datentd">' + studi.nachname + '</td><td  class="datentd">' + studi.vorname + '</td><td  class="datentd">' + matrikel + '</td><td  class="datentd">' + studi.alter + '</td><td  class="datentd">' + studi.geschlecht + '</td><td  class="datentd">' + studi.studiengang + '</td></tr>';
+            _response.write(line + "\n");
+        }
+    }
+    function search(query, _response) {
+        let studi = studiHomoAssoc[query["searchFor"]];
+        if (studi) {
+            let line = '<tr><td class="datentd">' + studi.nachname + '</td><td  class="datentd">' + studi.vorname + '</td><td  class="datentd">' + query + '</td><td  class="datentd">' + studi.alter + '</td><td  class="datentd">' + studi.geschlecht + '</td><td  class="datentd">' + studi.studiengang + '</td></tr>';
+            _response.write(line);
+        }
+        else {
+            _response.write("No Match");
+        }
+    }
+    function error() {
+        alert("Error");
     }
 })(Server || (Server = {}));
 //# sourceMappingURL=Server.js.map
--- Server.ts
+++ Server.ts
@@ -6,35 +6,93 @@ namespace Server {
         [key: string]: string;
     }
 
+    interface Studi {
+        vorname: string;
+        nachname: string;
+        martrikelnummer: number;
+        alter: number;
+        geschlecht: string;
+        studiengang: string;
+    }
+    
+    interface Studis {
+        [matrikel: string]: Studi;
+    }
+
+    let studiHomoAssoc: Studis = {};
     let port: number = process.env.PORT;
     if (port == undefined)
-        port = 8100;
-
-    let server: Http.Server = Http.createServer();
-    server.addListener("listening", handleListen);
+        port = 8200;
+    let server: Http.Server = Http.createServer((_request: Http.IncomingMessage, _response: Http.ServerResponse) => {
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+    });
     server.addListener("request", handleRequest);
     server.listen(port);
 
-    function handleListen(): void {
-        console.log("Ich höre?");
-    }
-
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
         console.log("Ich höre Stimmen!");
-
         let query: AssocStringString = Url.parse(_request.url, true).query;
-        let a: number = parseInt(query["a"]);
-        let b: number = parseInt(query["b"]);
-
-        for (let key in query) 
-            console.log(query[key]);
-
-        _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.setHeader("Access-Control-Allow-Origin", "*");
-
-        _response.write("Ich habe dich gehört<br/>");
-        _response.write("Das Ergebnis ist: " + (a + b));
-
+        console.log(query["command"]);
+        if (query["command"]) {
+            switch (query["command"]) {
+                case "insert":
+                    insert(query, _response);
+                    break;
+                case "refresh":
+                    refresh(_response);
+                    break;
+                case "search":
+                    search(query, _response);
+                    break;
+                default:
+                    error();
+            }
+        }
         _response.end();
+    }
+
+    function insert(query: AssocStringString, _response: Http.ServerResponse): void {
+        let obj: Studi = JSON.parse(query["data"]);
+        let _vorname: string = obj.vorname;
+        let _nachname: string = obj.nachname;
+        let _martrikelnummer: string = obj.martrikelnummer.toString();
+        let _alter: number = obj.alter;
+        let _geschlecht: string = obj.geschlecht;
+        let _studiengang: string = obj.studiengang;
+        let studi: Studi;
+        studi = {
+            vorname: _vorname,
+            nachname: _nachname,
+            martrikelnummer: parseInt(_martrikelnummer),
+            alter: _alter,
+            geschlecht: _geschlecht,
+            studiengang: _studiengang
+        };
+        studiHomoAssoc[_martrikelnummer] = studi;
+        _response.write("Daten empfangen");
+    }
+
+    function refresh(_response: Http.ServerResponse): void {
+        console.log(studiHomoAssoc);
+        for (let matrikel in studiHomoAssoc) {
+            let studi: Studi = studiHomoAssoc[matrikel];
+            let line: string = '<tr><td class="datentd">' + studi.nachname + '</td><td  class="datentd">' + studi.vorname + '</td><td  class="datentd">' + matrikel + '</td><td  class="datentd">' + studi.alter + '</td><td  class="datentd">' + studi.geschlecht + '</td><td  class="datentd">' + studi.studiengang + '</td></tr>';
+            _response.write(line + "\n");
+        }
+    }
+
+    function search(query: AssocStringString, _response: Http.ServerResponse): void {
+        let studi: Studi = studiHomoAssoc[query["searchFor"]];
+        if (studi) {
+            let line: string = '<tr><td class="datentd">' + studi.nachname + '</td><td  class="datentd">' + studi.vorname + '</td><td  class="datentd">' + query + '</td><td  class="datentd">' + studi.alter + '</td><td  class="datentd">' + studi.geschlecht + '</td><td  class="datentd">' + studi.studiengang + '</td></tr>';
+            _response.write(line);
+        } else {
+            _response.write("No Match");
+        }
+    }
+
+    function error(): void {
+        alert("Error");
     }
 }
